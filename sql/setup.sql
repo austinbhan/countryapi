5e4c368d5b31020705271dc97cf7c9abbc62db24
austinbhan <austinbhan@outlook.com>
2023-01-24 15:26:37 -0800

    SQL table created

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS countries CASCADE;
 
+CREATE TABLE countries (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    country VARCHAR NOT NULL,
+    capitol VARCHAR NOT NULL,
+    langue VARCHAR NOT NULL
+);
